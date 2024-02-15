be872858f64e631c582d1662d57f629e82d1d40f
남현우 <hy.nam@huray.net>
2024-02-15 10:43:26 +0900

    first commit

---
+++ src/main/java/org/example/examplebuilder/ExampleBuilderApplication.java
@@ -0,0 +1,13 @@
+package org.example.examplebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleBuilderApplication.class, args);
+    }
+
+}
